f9db63704c97da7d0dd38e950a08981eb661c454
glauber filho <glauber-94@hotmail.com>
2022-02-01 15:08:11 -0300

    criada rotas home e details

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import  Details from './pages/Details';
+import  Home  from './pages/Home';
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/:id" element={<Details/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Details/Details.js
@@ -0,0 +1,38 @@
+//GLOBAL - components from npm
+import React from 'react';
+
+//STYLES
+import './details.scss'
+
+//COMPONENTS
+
+
+//SERVICES - api, conectors...
+
+
+//GLOBAL STATE - redux, env...
+
+
+//ASSETS - icons, images...
+
+
+export default function Details() {
+    //GENERAL
+    
+    
+    //STATES
+    
+    
+    //REDUX - Selectors
+    
+    
+    //FUNCTIONS
+    
+    
+    //USE EFFECTS
+    
+    
+    return (
+        <div></div>
+    )
+}
