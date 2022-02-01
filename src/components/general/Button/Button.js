fd625dd66cf7d12f359b45b8a0e650091508512e
glauber filho <glauber-94@hotmail.com>
2022-02-01 17:37:53 -0300

    criado lista de filtros

--- src/components/general/Button/Button.js
+++ src/components/general/Button/Button.js
@@ -1,43 +1,49 @@
 //GLOBAL - components from npm
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 //STYLES
-import './button.scss'
+import "./button.scss";
 
 //COMPONENTS
 
-
 //SERVICES - api, conectors...
 
-
 //GLOBAL STATE - redux, env...
-
 
 //ASSETS - icons, images...
 import { ReactComponent as IconClose } from "../../../assets/Close.svg";
 
+export default function Button({ text, onClose, onClick }) {
+  //GENERAL
 
-export default function Button({text, onClose}) {
-    //GENERAL
-    
-    
-    //STATES
-    const [buttonActive, setButtonActive] = useState(false)
-    
-    
-    //REDUX - Selectors
-    
-    
-    //FUNCTIONS
-    
-    
-    //USE EFFECTS
-    
-    
-    return (
-        <div className={`button ${buttonActive ? 'active' : ''}`}>
-            <p>{text}</p>
-            {buttonActive && <IconClose onClick={onClose}/>}
-        </div>
-    )
+  //STATES
+  const [buttonActive, setButtonActive] = useState(false);
+
+  //REDUX - Selectors
+
+  //FUNCTIONS
+
+  //USE EFFECTS
+
+  return (
+    <div
+      className={`button ${buttonActive ? "active" : ""}`}
+      onClick={() => {
+          if(!buttonActive) {
+              setButtonActive(true);
+              onClick();
+          }
+      }}
+    >
+      <p>{text}</p>
+      {buttonActive && (
+        <IconClose
+          onClick={() => {
+            setButtonActive(false);
+            onClose();
+          }}
+        />
+      )}
+    </div>
+  );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,50 +1,80 @@
 //GLOBAL - components from npm
-import React from 'react';
+import React, { useState } from "react";
 
 //STYLES
-import './home.scss'
+import "./home.scss";
 
 //COMPONENTS
-import { PageHeader } from '../../components/layout'
+import { PageHeader } from "../../components/layout";
+import { Button } from "../../components/general";
 
 //SERVICES - api, conectors...
 
-
 //GLOBAL STATE - redux, env...
-
 
 //ASSETS - icons, images...
 
-
 export default function Home() {
-    //GENERAL
-    const categoryList = ["Ação", "Aventura", "Animação", "Comédia", "Crime", "Documentário", "Drama", "Família", "Fantasia", "História", "Terror", "Música", "Mistério", "Romance", "Ficção científica", "Cinema TV", "Thriller", "Guerra", "Faroeste"]
-    
-    
-    //STATES
-    
-    
-    //REDUX - Selectors
-    
-    
-    //FUNCTIONS
-    
-    
-    //USE EFFECTS
-    
-    
-    return (
-        <div className='home'>
-            <PageHeader/>
-            <section className="home-header">
-                <h1>Milhões de filmes, séries e pessoas para descobrir. Explore já.</h1>
-                <div className="home-header-filters">
-                    <h6>FILTRE POR:</h6>
-                    <div className="home-header-filters__list">
-                    
-                    </div>
-                </div>
-            </section>
+  //GENERAL
+  const categoryList = [
+    "Ação",
+    "Aventura",
+    "Animação",
+    "Comédia",
+    "Crime",
+    "Documentário",
+    "Drama",
+    "Família",
+    "Fantasia",
+    "História",
+    "Terror",
+    "Música",
+    "Mistério",
+    "Romance",
+    "Ficção científica",
+    "Cinema TV",
+    "Thriller",
+    "Guerra",
+    "Faroeste",
+  ];
+
+  //STATES
+  const [filtersList, setFiltersList] = useState([]);
+
+  //REDUX - Selectors
+
+  //FUNCTIONS
+
+  //USE EFFECTS
+
+  return (
+    <div className="home">
+      <PageHeader />
+      <section className="home-header">
+        <h1>Milhões de filmes, séries e pessoas para descobrir. Explore já.</h1>
+        <div className="home-header-filters">
+          <h6>FILTRE POR:</h6>
+          <div className="home-header-filters__list">
+            {categoryList.map((category, index) => (
+              <Button
+                key={index}
+                text={category}
+                onClick={() => {
+                  const arrayFilters = [...filtersList];
+                  arrayFilters.push(category);
+                  setFiltersList(arrayFilters);
+                }}
+                onClose={() => {
+                  const arrayFilters = filtersList.filter(
+                    (item) => item !== category
+                  );
+                  setFiltersList(arrayFilters);
+                }}
+              />
+            ))}
+          </div>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
