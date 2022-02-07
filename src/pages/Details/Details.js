6c89acf3ad316bff44259f5c0f54b3cfaac07935
glauber filho <glauber-94@hotmail.com>
2022-02-07 17:53:20 -0300

    responsivo do detalhe pronto

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,7 +1,7 @@
 //GLOBAL - components from npm
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {format} from 'date-fns';
+import { format } from "date-fns";
 
 //STYLES
 import "./details.scss";
@@ -39,19 +39,23 @@ export default function Details() {
     await getMovieTrailer();
     await getMovieRecommendations();
     setLoading(false);
-  }
+  };
 
   const getMovieCredits = async () => {
     const resposta = await API.get(`movie/${id}/credits`);
-    console.log('credits', resposta);
+    console.log("credits", resposta);
     if (resposta.erro) {
       return console.log(resposta.dados);
     } else {
       setCastList(resposta.dados.cast);
       const arrayCrew = [];
-      for(const item of resposta.dados.crew) {
-        if(item.job === 'Director' || item.job === 'Characters' || item.job === 'Screenplay') {
-          arrayCrew.push(item)
+      for (const item of resposta.dados.crew) {
+        if (
+          item.job === "Director" ||
+          item.job === "Characters" ||
+          item.job === "Screenplay"
+        ) {
+          arrayCrew.push(item);
         }
       }
       setCrewList(arrayCrew);
@@ -61,7 +65,7 @@ export default function Details() {
 
   const getMovieTrailer = async () => {
     const resposta = await API.get(`movie/${id}/videos`);
-    console.log('trailer', resposta);
+    console.log("trailer", resposta);
     if (resposta.erro) {
       return console.log(resposta.dados);
     } else {
@@ -71,7 +75,7 @@ export default function Details() {
 
   const getMovieRecommendations = async () => {
     const resposta = await API.get(`movie/${id}/recommendations`);
-    console.log('recommendations', resposta);
+    console.log("recommendations", resposta);
     if (resposta.erro) {
       return console.log(resposta.dados);
     } else {
@@ -81,7 +85,7 @@ export default function Details() {
 
   const getMovieDetail = async () => {
     const resposta = await API.get(`movie/${id}`);
-    console.log('details', resposta);
+    console.log("details", resposta);
     if (resposta.erro) {
       return console.log(resposta.dados);
     } else {
@@ -91,7 +95,7 @@ export default function Details() {
 
   //USE EFFECTS
   useEffect(() => {
-      getInfoDetails();
+    getInfoDetails();
   }, []);
 
   return (
@@ -107,30 +111,51 @@ export default function Details() {
               alt={movie.title}
             />
             <div className="details-header-information">
-                <div className="details-header-information__title">
-                    <h2>{movie.title} {` (${format(new Date(movie.release_date), "yyyy")})`}</h2>
-                </div>
-                <div className="details-header-information__details">
-                    <p>{format(new Date(movie.release_date), "dd/MM/yyyy")} • {movie.genres.map((item, index) => {
-                      if(index !== movie.genres.length - 1) {
-                        return item.name+', '
-                      } else {
-                        return item.name
-                      }
-                    })} • {(movie.runtime/60).toFixed(0)}h {(movie.runtime%60)}m</p>
-                </div>
-                <div className="details-header-information__synopsis">
-                    <h5>Sinopse</h5>
-                    <p>{movie.overview}</p>
-                </div>
-                <div className="details-header-information__list">
-                  {crewList.map((crew,index) => (
-                    <div key={index} className="details-header-information-crew">
-                      <h5>{crew.name}</h5>
-                      <p>{crew.job}</p>
+              <div className="details-header-information__title">
+                <h2>
+                  {movie.title}{" "}
+                  {` (${format(new Date(movie.release_date), "yyyy")})`}
+                </h2>
+              </div>
+              <div className="details-header-information__details">
+                <p>
+                  {format(new Date(movie.release_date), "dd/MM/yyyy")} •&nbsp;
+                </p>
+                <p>{movie.genres.map((item, index) => {
+                    if (index !== movie.genres.length - 1) {
+                      return item.name + ", ";
+                    } else {
+                      return item.name;
+                    }
+                  })} •&nbsp;</p>
+                  <p>{(movie.runtime / 60).toFixed(0)}h {movie.runtime % 60}m</p>
+              </div>
+              <div className="details-header-information__assessment">
+                <div className="circle-wrap">
+                  <div className="circle">
+                    <div className="mask half">
+                      <div className="fill" style={{transform: `rotate(${180*(movie.vote_average * 10) / 100}deg)`}}></div>
                     </div>
-                  ))}
+                    <div className="mask full" style={{transform: `rotate(${180*(movie.vote_average * 10) / 100}deg)`}}>
+                      <div className="fill" style={{transform: `rotate(${180*(movie.vote_average * 10) / 100}deg)`}}></div>
+                    </div>
+                  <div className="inside-circle">{movie.vote_average * 10}%</div>
+                  </div>
                 </div>
+                <p>Avaliação dos usuários</p>
+              </div>
+              <div className="details-header-information__synopsis">
+                <h5>Sinopse</h5>
+                <p>{movie.overview}</p>
+              </div>
+              <div className="details-header-information__list">
+                {crewList.map((crew, index) => (
+                  <div key={index} className="details-header-information-crew">
+                    <h5>{crew.name}</h5>
+                    <p>{crew.job}</p>
+                  </div>
+                ))}
+              </div>
             </div>
           </section>
           <main className="details-main">
@@ -138,20 +163,32 @@ export default function Details() {
               <h3>Elenco original</h3>
               <div className="details-main-cast__list">
                 {castList.map((cast, index) => (
-                  <CardCast key={index} name={cast.name} image={cast.profile_path} character={cast.character}/>
+                  <CardCast
+                    key={index}
+                    name={cast.name}
+                    image={cast.profile_path}
+                    character={cast.character}
+                  />
                 ))}
               </div>
             </div>
             <div className="details-main-trailer">
               <h3>Trailer</h3>
-              <img  src={`https://www.themoviedb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.title}/>
+              <img
+                src={`https://www.themoviedb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+              />
             </div>
             <div className="details-main-recommendation">
               <h3>Recomendações</h3>
               <div className="details-main-recommendation__list">
                 {recommendations.map((recommendation, index) => (
-                  <Card key={index} title={recommendation.title} image={recommendation.poster_path} date={recommendation.release_date}/>
+                  <Card
+                    key={index}
+                    title={recommendation.title}
+                    image={recommendation.poster_path}
+                    date={recommendation.release_date}
+                  />
                 ))}
               </div>
             </div>
